5fcc8d611eb4ae6d6ef74c8c4e1a697c32122298
LJY2000-max <n26132097@gs.ncku.edu.tw>
2024-11-10 17:22:27 +0800

    The main computation logic has been added to argmax.s, enabling it to pass the test cases test_argmax_length_1 and test_argmax_standard.

--- src/argmax.s
+++ src/argmax.s
@@ -22,16 +22,41 @@
 #   - Terminates program with exit code 36 if array length < 1
 # =================================================================
 argmax:
+    # Check array_length ≥ 1
     li t6, 1
     blt a1, t6, handle_error
 
+    # Load number from memory
     lw t0, 0(a0)
 
+    #li t1, 0
+    #li t2, 1
+
+    # Count array index
     li t1, 0
-    li t2, 1
+    li t2, 0
 loop_start:
     # TODO: Add your own implementation
+    # Load number from memory
+    lw t3, 0(a0)
 
+    bge t0, t3, loop_end
+
+    # Update the max value
+    addi t0, t3, 0
+    # Update the argmax
+    addi t1, t2, 0
+loop_end:
+    # Circular variables minus 1
+    addi a1, a1, -1
+    # Address of next array number
+    addi a0, a0, 4
+    # Array index plus 1
+    addi t2, t2, 1
+    bne zero, a1, loop_start
+    # return the argmax
+    addi a0, t1, 0
+    jr ra
 handle_error:
     li a0, 36
     j exit
