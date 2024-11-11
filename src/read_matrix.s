c79da3b33ccba1f7be52f17583c4c8cfe3929017
LJY2000-max <n26132097@gs.ncku.edu.tw>
2024-11-11 15:03:40 +0800

    The multiply function has been added to read_matrix.s, enabling it to pass the test cases test_read_1, test_read_2, test_read_3, test_read_fail_fclose, and test_read_fail_malloc.

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -76,6 +76,11 @@ read_matrix:
 
     # mul s1, t1, t2   # s1 is number of elements
     # FIXME: Replace 'mul' with your own implementation
+    li s1, 0
+    multiply:
+    add s1, s1, t2
+    addi t1, t1, -1
+    bgt t1, zero, multiply
 
     slli t3, s1, 2
     sw t3, 24(sp)    # size in bytes
